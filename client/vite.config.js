00facf593b8dceefd27183121099fe75187e616b
Rudd21 <yackovschool@gmail.com>
2026-02-09 22:37:25 +0200

    remove https

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import fs from 'fs';
 
 export default defineConfig({
     plugins: [react(), tailwindcss()],
     server: {
-        https: {
-            key: fs.readFileSync('./localhost-key.pem'),
-            cert: fs.readFileSync('./localhost.pem'),
-        },
         port: 5000,
     }
 })
