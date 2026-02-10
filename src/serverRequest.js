2877e41969639383cb68530b7e3e193eea98eb48
Rudd21 <yackovschool@gmail.com>
2026-02-10 18:00:13 +0200

    change upload dir

--- src/serverRequest.js
+++ src/serverRequest.js
@@ -1,3 +1,3 @@
 export const apiRequest = "https://seedra-backend.vercel.app"
 export const clientRequest = "https://seedra-food.vercel.app"
-export const uploads = "../public/"
+export const uploads = "."
