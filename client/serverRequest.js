dc045622227c6f85a327b31e088a35b5c7f7c201
Rudd21 <yackovschool@gmail.com>
2026-02-10 12:10:28 +0200

    fix: rename api to server to bypass auto-functions

--- client/serverRequest.js
+++ client/serverRequest.js
@@ -1,2 +1,2 @@
-export const apiRequest = "https://seedra-food.vercel.app/api"
+export const apiRequest = "https://seedra-food.vercel.app/server"
 export const clientRequest = "https://seedra-food.vercel.app"
